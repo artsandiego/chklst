0aa1870315a4c463a73245eda8a6e94d2010907e
Art San Diego <crtved.artsandiego@gmail.com>
2021-04-02 14:58:42 +0800

    Social Share Image

--- pages/index.js
+++ pages/index.js
@@ -49,6 +49,21 @@ export default function Home({ resourcesData }) {
       <Head>
         <title>C H K L S T.</title>
         <link rel="icon" href="/favicon.png" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="A one-stop repository of different resources for Front-End Developers and UI Designers."/>
+        <meta name="keywords" content="frontend, tools, crtved, artsandiego, website, ui, ux, web design, tips, resources"/>
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://chklst.vercel.app"/>
+        <meta property="og:title" content="CHKLST."/>
+        <meta property="og:description" content="A one-stop repository of different resources for Front-End Developers and UI Designers."/>
+        <meta property="og:image" content="/social-image.png"/>
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://chklst.vercel.app"/>
+        <meta property="twitter:title" content="CHKLST."/>
+        <meta property="twitter:description" content="A one-stop repository of different resources for Front-End Developers and UI Designers."/>
+        <meta property="twitter:image" content="/social-image.png" />
       </Head>
       <header className="flex flex-col items-center justify-center relative mobile:px-4">
         <div className="mt-14">
